c2a2216b149ae9aea26adc6009a196747492a513
James Harvey <jjharvey@sententix.com>
2020-03-29 14:02:19 -0400

    go routines test

--- test.go
+++ test.go
@@ -2,7 +2,16 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
+
+func series(n float64, m float64, r chan float64) {
+	s := 0.0
+	for i := n; i > 0.0; i -= m {
+		s += 1.0 / i
+	}
+	r <- s
+}
 
 func fib(n int) int {
 	if n < 2 {
@@ -12,7 +21,12 @@ func fib(n int) int {
 }
 
 func main() {
-	for i := 1; i < 10; i++ {
-		fmt.Println(fib(i))
+	n, m, r, s := 10000000000000.0, 8.0, make(chan float64), 0.0
+	for i := 0.0; i < m; i++ {
+		go series(n-i, m, r)
 	}
+	for i := 0.0; i < m; i++ {
+		s += <-r
+	}
+	fmt.Println(s - math.Log(n))
 }
