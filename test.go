341f4707e92de4850e09696e09e553bd312409c4
James Harvey <jjharvey@sententix.com>
2020-04-24 16:46:33 -0400

    multithreaded test harness

--- test.go
+++ test.go
@@ -3,48 +3,67 @@ package main
 import (
 	"flag"
 	"fmt"
+	"sync"
 )
 
-func printTXT(path string, cols map[string][2]int, comment string) {
+var wg sync.WaitGroup
+
+func printTXT(path string, cols map[string][2]int, comment string, detail bool) {
 	in, err, sig := ReadTXT(path, cols, comment)
 	defer close(sig)
+	rows := 0
 	for txt := range in {
-		fmt.Println(txt)
+		if rows++; detail {
+			fmt.Println(txt)
+		}
 	}
 	if e := <-err; e != nil {
 		fmt.Println(e)
 	}
+	fmt.Printf("read %d rows from TXT file %q\n", rows, path)
 }
 
-func printCSV(path string, sep rune, comment string) {
+func printCSV(path string, sep rune, comment string, detail bool) {
 	in, err, sig := ReadCSV(path, nil, sep, comment)
 	defer close(sig)
+	rows := 0
 	for csv := range in {
-		fmt.Println(csv)
+		if rows++; detail {
+			fmt.Println(csv)
+		}
 	}
 	if e := <-err; e != nil {
 		fmt.Println(e)
 	}
+	fmt.Printf("read %d rows from CSV file %q\n", rows, path)
 }
 
-func peekPrint(path string) {
+func peekPrint(path string, detail bool) {
 	if dig, e := PeekCSV(path); e != nil {
 		fmt.Println(e)
 	} else if dig.sep > '\x00' {
 		fmt.Println(dig)
-		printCSV(path, dig.sep, dig.comment)
+		printCSV(path, dig.sep, dig.comment, detail)
 	} else {
 		fmt.Println(dig)
-		printTXT(path, map[string][2]int{"~raw": {1, len(dig.preview[0])}}, dig.comment)
+		printTXT(path, map[string][2]int{"~raw": {1, len(dig.preview[0])}}, dig.comment, detail)
 	}
 }
 
 func main() {
-	path := flag.String("f", "test.csv", fmt.Sprintf("specify `pathname`"))
+	detail := flag.Bool("d", false, fmt.Sprintf("specify detailed output"))
 	flag.Usage = func() {
 		fmt.Printf("command usage: gt <flags>\n")
 		flag.PrintDefaults()
 	}
 	flag.Parse()
-	peekPrint(*path)
+
+	for _, file := range flag.Args() {
+		wg.Add(1)
+		go func(f string, d bool) {
+			defer wg.Done()
+			peekPrint(f, d)
+		}(file, *detail)
+	}
+	wg.Wait()
 }
